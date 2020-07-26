65f316b9eb8808a98698d39392d9b903b1b9d68b
Ed <eddeighton@hotmail.co.uk>
2020-07-26 13:12:30 +0100

    work in progress port to 64bit

--- cmake/boost_include.cmake
+++ cmake/boost_include.cmake
@@ -12,17 +12,27 @@ set(BOOST_REQUIRED_VERSION 1.73.0 )
 set(Boost_USE_STATIC_LIBS ON)
 set(Boost_USE_MULTITHREADED ON)
 set(Boost_USE_STATIC_RUNTIME OFF)
-find_package( Boost 1.73.0 REQUIRED QUIET COMPONENTS 
-    program_options
-    date_time
-    random
-    filesystem
-    system
-    iostreams
-    serialization
-	timer )
+set(Boost_ARCHITECTURE "-x64")
+set(Boost_COMPILER "-vc166")
+#set(Boost_DEBUG ON)
+#find_package( Boost 1.73.0 REQUIRED QUIET COMPONENTS 
+#    program_options
+#    date_time
+#    random
+#    filesystem
+#    system
+#    iostreams
+#    serialization
+#	timer )
+    
+#if(Boost_FOUND)
+#    message(STATUS "Found boost ok" )
+#else()
+#    message(FATAL_ERROR "Failed to find boost")
+#endif()
 
 function( compiler_define_boost targetname )
+	target_compile_definitions( ${targetname} PRIVATE -DBOOST_ALL_NO_LIB -D_BOOST_ALL_NO_LIB )
 	target_compile_definitions( ${targetname} PRIVATE -DBOOST_ENABLE_ASSERT_HANDLER -D_BOOST_ENABLE_ASSERT_HANDLER )
 	target_compile_definitions( ${targetname} PRIVATE -DBOOST_CONFIG_SUPPRESS_OUTDATED_MESSAGE -D_BOOST_CONFIG_SUPPRESS_OUTDATED_MESSAGE )
 endfunction( compiler_define_boost )
